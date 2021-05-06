48c4f378d48ea670e2af2493d826ea83253bd94e
kOF.Readie <kof.readie@outlook.com>
2021-05-06 21:37:15 +0100

    Update index.php

--- discord/index.php
+++ discord/index.php
@@ -1,6 +1,2 @@
-<?php $DOCUMENT_ROOT = $_SERVER["DOCUMENT_ROOT"];require_once "{$DOCUMENT_ROOT}/assets/php/main.php"; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head><?php echo execAndRead("{$DOCUMENT_ROOT}/assets/php/head.php"); ?></head>
-<body><meta http-equiv="refresh" content="0; URL='https://discord.gg/fk3G2c6'"/>
-</html>
+<?php
+header('Location: https://discord.gg/fk3G2c6', true, 301);
